272bc354a694f284904598786751112ff44859fd
thalerjonathan <thaler.jonathan@gmail.com>
2014-03-05 15:24:49 +0100

    added common.h some comments

---
+++ kernel/src/common/common.h
@@ -0,0 +1,15 @@
+/*
+ * common.h
+ *
+ *  Created on: 05.03.2014
+ *      Author: Thaler
+ */
+
+#ifndef COMMON_H_
+#define COMMON_H_
+
+#include <boss.h>
+
+
+
+#endif /* COMMON_H_ */
--- kernel/src/scheduler/scheduler.c
+++ kernel/src/scheduler/scheduler.c
@@ -11,17 +11,25 @@
 int32_t
 createTask( task_func entryPoint )
 {
+	// TODO: create new task-entry in structure
+	// TODO: PC = entryPoint
+
 	return entryPoint( 0 );
 }
 
 int32_t
 fork()
 {
+	// TODO: clone the task-structure of the current running process
+	// TODO: child needs to return 0 (r0 = 0) and parent > 0 (r0 = pid)
+
 	return 0;
 }
 
 int32_t
 sleep( uint32_t millis )
 {
+	// TODO: tell scheduler to keep it not ready for the next millis
+
 	return 0;
 }

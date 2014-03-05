d36b8fb0e5fbab6434fb53f1296765633769d386
michaelfaisst <michael.faisst@students.fhv.at>
2014-03-05 10:32:21 +0100

    Timer Defines added

--- kernel/src/timer/gptimer.h
+++ kernel/src/timer/gptimer.h
@@ -8,8 +8,17 @@
 #ifndef GPTIMER_H_
 #define GPTIMER_H_
 
-#define GPTIMER1 0x48318000
+#define GPTIMER1_BASE 0x48318000
+#define GPTIMER2_BASE 0x49032000
+#define GPTIMER_TMAR 0x038 // compare value
+#define GPTIMER_TIER 0x01C // controls interrupt events (match, overflow, capture)
+#define GPTIMER_TCLR 0x024 //
+#define GPTIMER_TCRR 0x028 // timer counter
+#define GPTIMER_TLDR 0x02C // timer load value
 
+#define GPTIMER_MATCH 1
+#define GPTIMER_OVF (1<<1)
+#define GPTIMER_CAPT (1<<2)
 
 
 #endif /* GPTIMER_H_ */

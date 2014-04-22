8b104da506e67ad904eaa5280df9fdbd41255b75
Andreas Sinz <andreas.sinz@aon.at>
2014-04-22 22:36:33 +0200

    Fixed serial driver

--- DriverInfrastructure/driver/serial/serial_driver.c
+++ DriverInfrastructure/driver/serial/serial_driver.c
@@ -7,12 +7,26 @@
 
 
 #include "serial_driver.h"
+#include "../../common/common.h"
 #include "../return_code.h"
 #include "../ioctl.h"
 
 #define NULL ((void*)0)
 
 #define UART3_BASE 0x49020000
+
+#define DLL_REG 0x000 // Divisor latches lows
+#define DLH_REF 0x004 // Divisor latches high // Interrupt enable register ??
+//#define IER_REG 0x004 // Interrupt enable register
+#define MDR1_REG 0x020 // Mode definition register
+#define FCR_REG 0x008 // FIFO control register
+#define SSR_REG 0x044 // status register [0] = 1 FIFO = Full
+#define TLR_REG 0x01C // Trigger level register
+#define LCR_REG 0x00C // Line control register
+#define MCR_REG 0x010 // Modem control register
+#define LSR_REG 0x014 // Line status register
+#define MSR_REG 0x018 // Modem status register
+#define SCR_REG 0x040 // Supplementary control register
 
 #pragma pack()
 
@@ -42,14 +56,15 @@ volatile struct UART_CONF *com_port= (UART_CONF_t) UART3_BASE;
 static RETURN_CODE openSerial(Driver_t *self) {
 	int baud_divisor=312;
 
-	com_port->mdr1 = 0x7;  // MODE_SELECT=DISABLE
-	com_port->lcr = 0x80 | 0x03;
-	com_port->rbr = baud_divisor & 0xff;
-	com_port->rbr = (baud_divisor >> 8) & 0xff;
-    	com_port->lcr = 0x03;
-	com_port->mcr = 0x01 | 0x02;
-	com_port->fcr = 0x01 | 0x02 | 0x04;
-	com_port->mdr1 = 0;
+	reg32w(UART3_BASE, MDR1_REG, 0x07);
+	reg32w(UART3_BASE, LCR_REG, 0x80|0x03);
+	reg32w(UART3_BASE, DLL_REG, (baud_divisor & 0xff));
+	reg32w(UART3_BASE, DLH_REF, (baud_divisor >> 8) & 0xff);
+	reg32w(UART3_BASE, LCR_REG, 0x03);
+	reg32w(UART3_BASE, MCR_REG, 0x01 | 0x02 );
+	reg32w(UART3_BASE, FCR_REG, 0x01 | 0x02 | 0x04);
+	reg32w(UART3_BASE, MDR1_REG, 0x0);
+
 	return SUCCESS;
 }
 
@@ -66,8 +81,8 @@ static RETURN_CODE writeSerial(Driver_t *self, void *message) {
 	char *tmp = message;
 
 	while (*tmp){
-        while ((com_port->lsr & 0x20) == 0);
-             com_port->rbr= (*tmp++);
+	while ((reg32r(UART3_BASE,LSR_REG) & 0x20) == 0);
+ 	 	 reg32w(UART3_BASE,0,(*tmp++));
  	}
 	return SUCCESS;
 }
@@ -77,7 +92,7 @@ static void* readSerial(Driver_t *self) {
 }
 
 Driver_t *createSerialDriver() {
-	Driver_t *serialDriver = (Driver_t*)malloc(sizeof(Driver_t));
+	Driver_t *serialDriver = (Driver_t *)malloc(sizeof(Driver_t *));
 	serialDriver->ioctl = &ioctlSerial;
 	serialDriver->open = &openSerial;
 
